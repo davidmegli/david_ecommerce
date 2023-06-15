7d72bb3a758e26ed69adb89b9d5b1fed43f3fbc4
davidmegli <verbatimdvd91@gmail.com>
2023-06-15 18:27:36 +0200

    Edited static root

--- core/settings.py
+++ core/settings.py
@@ -128,11 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static") # BASE_DIR is the root folder of the project
-] 
+STATIC_ROOT = os.path.join(BASE_DIR, "static") # BASE_DIR is the root folder of the project
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
